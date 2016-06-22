58924a2fb7f74f76d298df7cd61b83ee3c978611
davidson <davidson@weizmann.ac.il>
2016-06-22 14:24:22 +0300

    lang/en/davidson (new) strings we added

---
+++ lang/en/davidson.php
@@ -0,0 +1,75 @@
+<?php
+
+$string['notenrolledtoanycourse'] = 'You are not enrolled to any course';
+$string['noaccess'] = 'You have no access';
+$string['activityiscurrentlyunavailable'] = 'This activity is currently unavailable until you finish previous activity';
+$string['footernotes'] = 'Footer notes';
+$string['oursitename'] = 'Davidson Institute';
+$string['messagesdisabled'] = 'Messaging system is disabled';
+$string['enablemessage'] = 'message system';
+$string['enablemessageyes'] = 'enabled';
+$string['enablemessageno'] = 'disabled';
+$string['disablemsg'] = 'disable messaging';
+$string['enablemsg'] = 'enable messaging';
+$string['logincheck'] = 'Please check your username and password';
+$string['logincheckpassword'] = 'You are typing in Hebrew';
+$string['emailsend'] = 'Send email';
+$string['notanswered'] = 'Did not answer';
+$string['send_email'] = 'Send email';
+$string['forgotpasssupport'] = 'support';
+$string['pregroup'] = 'grouping_';
+$string['groupingsdelete'] = 'groupings delete';
+$string['autocreategroupings'] = 'Auto create groupings';
+$string['clozeeditor_dlg:chooseclozeformat'] = 'Please choose a Cloze sub-question format:';
+$string['clozeeditor_dlg:encode'] = 'Encode';
+$string['clozeeditor_dlg:titleclozeeditor'] = 'Embedded answers (CLOZE) item';
+$string['clozeeditor_dlg:output'] = 'Output';
+$string['pregroup'] = 'grouping_';
+$string['groupingsdelete'] = 'groupings delete';
+$string['autocreategroupings'] = 'Automatic groupings creation';
+$string['hellotoyou'] = 'Hello';
+$string['abouttodeletegroupings'] = 'About to delete groupings: {$a}<br/>';
+$string['showaquestiongrades'] = 'Show questions and grades';
+$string['showallandgrade'] = 'Show all questions on one page';
+$string['showquestionsanswers'] = 'Summary: questions and your answers';
+
+$string['goodluck']= 'Good luck';
+$string['enteridnumber'] = 'Please enter a valid IDNumber';
+$string['invalididnumber'] = 'Invalid IDNumber';
+$string['summaryofattempt'] = 'Summary of your attempt';
+$string['emailstop'] = 'Do not send eMails';
+$string['male'] = 'Male';
+$string['female'] = 'Female';
+$string['gender'] = 'Gender';
+$string['youaresuspended'] = 'Please finish the registration process for course "{$a}".' ;
+$string['movetogroup'] = 'move to group';
+
+$string['beaver_certificate'] = 'Certificate';
+$string['beaver_shchva'] = 'shchva0';
+$string['beaver_shchva0'] = 'shchva1';
+$string['beaver_shchva1'] = 'shchva2';
+$string['beaver_shchva2'] = 'shchva3';
+$string['mikra'] = 'i=information';
+
+$string['chooselayout'] = 'Choice layout';
+$string['layout'] = 'Question layout';
+$string['layoutvertical'] = 'Vertical layout';
+$string['layouthorizontal'] = 'Horizontal layout';
+
+$string['forgotpassuppot'] = 'Or call 08-9378370';
+$string['forgotpassuppot1'] = 'For technical support, email us: davidson@weizmann.ac.il';
+$string['searchpass'] = 'Send email to reset password';
+
+$string['davidsonhdr'] = 'Visual settings';
+$string['quizvisualsettings'] = 'parameters';
+$string['quizvisualsettingshelp'] = 'Set each parameter to 1=display or 0=hide';
+$string['configquizvisualsettings'] = 'info=1,info_flag=1,info_question_number=1,info_grade=1,approvesubmit=1,quizsummary=1';
+$string['mouseoveranswer'] = 'put mouse over your answer to see feedback';
+$string['usernamehint'] = 'your email';
+
+$string['eventuserreturned'] = 'User returned';
+$string['toc'] = 'Contents';
+$string['notregisteredyet'] = 'You are not registered to any course yet';
+$string['forregistration'] = 'For registration go to Davidson site';
+$string['personaldetails'] = 'personal details';
+$string['usersdetails'] = 'Users details';
